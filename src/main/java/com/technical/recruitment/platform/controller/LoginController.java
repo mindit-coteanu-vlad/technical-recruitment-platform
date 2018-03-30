ff046d59e7e74e6dd4b98d0ca86164ac92f258ed
Coteanu Vlad <vlad.coteanu@mindit.ro>
2018-03-30 14:10:59 +0300

    Fix bug

--- src/main/java/com/technical/recruitment/platform/controller/LoginController.java
+++ src/main/java/com/technical/recruitment/platform/controller/LoginController.java
@@ -6,22 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping(value = "/trp")
 public class LoginController {
 
     @Autowired
     private UserService userService;
 
-    @Autowired
     @RequestMapping(value="/loginCandidate", method = RequestMethod.GET)
-    public ResponseEntity login(@RequestParam String userCode) {
+    public ResponseEntity login(@PathVariable(value = "user-code") String userCode) {
 
         Optional<UserDTO> optional = userService.getUserByToken(userCode);
         UserDTO userDTO = null;
